f61a386c6d84a227aa1d2ffc192281cec4df8f01
JasonWeng <jweng@equuscs.com>
2020-06-05 16:26:34 +0800

    revise balck circle

--- black circle.glsl
+++ black circle.glsl
@@ -1,18 +1,12 @@
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     // Normalized pixel coordinates (from 0 to 1)
-    vec2 uv = fragCoord / iResolution.xy;
-    
-    float r = 0.3f;
-    uv -= 0.5f;
+    vec2 uv = fragCoord/iResolution.xy;
+	uv -= 0.5f;
     uv.x *= iResolution.x / iResolution.y;
-    
-    if(sqrt(uv.x * uv.x + uv.y * uv.y) > r)
-    {
-    	fragColor = vec4(1.0);
-    }
-    else
-    {
-        fragColor = vec4(vec3(0), 1.0);
-    }
+	
+    float r = 0.3f;
+    float l = sqrt(uv.x * uv.x + uv.y * uv.y);
+    vec3 c = l > r ? vec3(1) : vec3(0);
+    fragColor = vec4(c,1.0);
 }
