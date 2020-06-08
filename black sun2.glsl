f8bac459478a5e6fc18752f1fd129183e2479480
JasonWeng <jweng@equuscs.com>
2020-06-08 17:13:34 +0800

    black sun2 - research multiple circle with different center

--- black sun2.glsl
+++ black sun2.glsl
@@ -1,6 +1,6 @@
-float BlackCircle(vec2 uv, float displace, float r, float blur)
+float BlackCircle(vec2 uv, vec2 center, float r, float blur)
 {
-    //uv -= displace;
+    uv -= center;
     float l = sqrt(uv.x * uv.x + uv.y * uv.y);
     float c = smoothstep(r, r + blur, l);
     return c;
@@ -9,11 +9,13 @@ float BlackCircle(vec2 uv, float displace, float r, float blur)
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     // Normalized pixel coordinates (from 0 to 1)
-    vec2 uv = fragCoord/iResolution.xy;
+    vec2 uv = fragCoord / iResolution.xy;
     uv -= 0.5f;
     uv.x *= iResolution.x / iResolution.y;
-    //uv -= 0.5f;
+    //uv.y *= iResolution.y / iResolution.x;
     
-    float c = BlackCircle(uv, 0.5f, 0.4f, 0.015f);
-    fragColor = vec4(vec3(c),1.0);
+    float c1 = BlackCircle(uv, vec2(-0.3f, 0.5f), 0.15f, 0.015f);
+    float c2 = BlackCircle(uv, vec2(0.5f, 0), 0.3f, 0.015f);
+    float c = c1 + c2;
+    fragColor = vec4(vec3(c), 1.0f);
 }
